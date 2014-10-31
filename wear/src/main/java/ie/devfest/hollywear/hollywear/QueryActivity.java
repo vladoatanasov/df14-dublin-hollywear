ad1925d7edc752454c35e304651ae1c096ecac94
Sebastian Mauer <mauimauer@gmail.com>
2014-10-31 02:33:41 +0100

    Create Project (mobile and wear modules) in Android Studio

---
+++ wear/src/main/java/ie/devfest/hollywear/hollywear/QueryActivity.java
@@ -0,0 +1,24 @@
+package ie.devfest.hollywear.hollywear;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class QueryActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
